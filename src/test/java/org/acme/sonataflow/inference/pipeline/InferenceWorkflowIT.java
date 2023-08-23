6b2160d10755b3d5a7486384beb6145fc417b6c3
Helber Belmiro <helber.belmiro@gmail.com>
2023-08-23 17:44:48 -0300

    Added test

---
+++ src/test/java/org/acme/sonataflow/inference/pipeline/InferenceWorkflowIT.java
@@ -0,0 +1,25 @@
+package org.acme.sonataflow.inference.pipeline;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTest
+class InferenceWorkflowIT {
+
+    @Test
+    void test() {
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when().post("/inference")
+                .then()
+                .statusCode(201)
+                .body("workflowdata.preProcessingData", is("Data was pre processed"))
+                .body("workflowdata.modelServerData", is("modelServer data"))
+                .body("workflowdata.postProcessingData", is("Data was post processed"));
+    }
+}
