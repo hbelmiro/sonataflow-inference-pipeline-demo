cee92df21a8d2b07768c225d6cad83c90af04fec
Helber Belmiro <helber.belmiro@gmail.com>
2023-08-30 17:01:40 -0300

    Added image overlay state (hard coed for now)
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- src/test/java/org/acme/sonataflow/inference/pipeline/InferenceWorkflowTest.java
+++ src/test/java/org/acme/sonataflow/inference/pipeline/InferenceWorkflowTest.java
@@ -4,9 +4,11 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Paths;
 import java.time.LocalDate;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
@@ -23,6 +25,9 @@ class InferenceWorkflowTest {
                 .body("workflowdata.preProcessingData", is("Data was pre processed"))
                 .body("workflowdata.modelServerData", is("modelServer data"))
                 .body("workflowdata.postProcessingData", is("Data was post processed"))
-                .body("workflowdata.today", is(LocalDate.now().getDayOfMonth()));
+                .body("workflowdata.today", is(LocalDate.now().getDayOfMonth()))
+                .body("workflowdata.overlayed_image", is("overlayed_image.jpg"));
+
+        assertThat(Paths.get("overlayed_image.jpg")).exists();
     }
 }
