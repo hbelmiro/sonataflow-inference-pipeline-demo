376fe23d06f82d0e8d726b7d86a338ec2cb044bb
Helber Belmiro <helber.belmiro@gmail.com>
2023-08-31 09:52:48 -0300

    Added image overlay state (hard coded for now) (#8)
    * Added image overlay state (hard coed for now)
    
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>
    
    * Fixed typo
    
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>
    
    ---------
    
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- src/test/java/org/acme/sonataflow/inference/pipeline/InferenceWorkflowTest.java
+++ src/test/java/org/acme/sonataflow/inference/pipeline/InferenceWorkflowTest.java
@@ -4,16 +4,21 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
 class InferenceWorkflowTest {
 
     @Test
-    void test() {
+    void test() throws IOException {
         given()
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
@@ -23,6 +28,14 @@ class InferenceWorkflowTest {
                 .body("workflowdata.preProcessingData", is("Data was pre processed"))
                 .body("workflowdata.modelServerData", is("modelServer data"))
                 .body("workflowdata.postProcessingData", is("Data was post processed"))
-                .body("workflowdata.today", is(LocalDate.now().getDayOfMonth()));
+                .body("workflowdata.today", is(LocalDate.now().getDayOfMonth()))
+                .body("workflowdata.overlaid_image", is("overlaid_image.jpg"));
+
+        Path overlaidImage = Paths.get("overlaid_image.jpg");
+        try {
+            assertThat(overlaidImage).exists();
+        } finally {
+            Files.delete(overlaidImage);
+        }
     }
 }
