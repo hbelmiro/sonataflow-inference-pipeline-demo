366f1a8d9c148b16ed00276f2aba19ee94ee2319
Helber Belmiro <helber.belmiro@gmail.com>
2023-09-05 13:55:41 -0300

    Created kserve request payload (#12)

--- src/test/java/org/acme/sonataflow/inference/pipeline/InferenceWorkflowTest.java
+++ src/test/java/org/acme/sonataflow/inference/pipeline/InferenceWorkflowTest.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDate;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,10 +25,18 @@ class InferenceWorkflowTest {
                 .then()
                 .statusCode(201)
                 .body("workflowdata.preProcessingData", is("Data was pre processed"))
+                .body("workflowdata.image", is("coco_image.jpg"))
+                .body("workflowdata.kserve_payload", is("coco_image.json"))
                 .body("workflowdata.model_server_data.segmented_image", is("test_image_house.jpg"))
                 .body("workflowdata.postProcessingData", is("Data was post processed"))
-                .body("workflowdata.today", is(LocalDate.now().getDayOfMonth()))
                 .body("workflowdata.overlaid_image", is("overlaid_image.jpg"));
+
+        Path kserveRequestPayload = Paths.get("coco_image.json");
+        try {
+            assertThat(kserveRequestPayload).exists();
+        } finally {
+            Files.delete(kserveRequestPayload);
+        }
 
         Path overlaidImage = Paths.get("overlaid_image.jpg");
         try {
